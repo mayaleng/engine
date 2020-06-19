8582426d9fba979fcc7b1c135578fb8fb47c0f75
Alexander Baquiax <alex@baquiax.dev>
2020-06-18 20:41:45 -0600

    feat(tests): Adding tests for rules

--- internal/translator/test/rules.go
+++ internal/translator/test/rules.go
@@ -2,14 +2,58 @@ package test
 
 import (
 	"context"
+	"encoding/json"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"mayaleng.org/engine/internal/platform/data"
 )
 
+var testRule = `
+{
+	"source_language": "espaol",
+	"target_language": "kaqchikel",
+	"pattern": "VERB,ADV,ADJ",
+	"details": [
+	  {
+	    "tag": "VERB",
+	    "type": "",
+	    "properties": {}
+	  },
+	  {
+	    "tag": "ADV",
+	    "type": "",
+	    "properties": {}
+	  },
+	  {
+	    "tag": "ADJ",
+	    "type": "",
+	    "properties": {}
+	  }
+	],
+	"output": [
+	  {
+	    "type": "direct-translation",
+	    "value": "{{(index .Words 2).Lemma}}"
+	  },
+	  {
+	    "type": "literal",
+	    "value": " "
+	  },
+	  {
+	    "type": "direct-translation",
+	    "value": "{{(index .Words 1).Lemma}}"
+	  },
+	  {
+	    "type": "direct-translation",
+	    "value": "{{(index .Words 0).Lemma}}"
+	  }
+	]
+      }
+
+`
+
 // RulesTest is for testing purpose
 type RulesTest struct {
-
 }
 
 // New always returns a valid ObjectID
@@ -20,7 +64,22 @@ func (r RulesTest) New(ctx context.Context, ruleStruct data.NewRule) (*primitive
 
 // Find always returns a 1-length array of rules
 func (r RulesTest) Find(ctx context.Context, sourceLanguage, targetLanguage, pattern string) ([]data.Rule, error) {
-	return []data.Rule{}, nil
+	var rule = data.Rule{}
+	var rules = make([]data.Rule, 0)
+
+	if pattern != "VERB,ADV,ADJ" {
+		return rules, nil
+	}
+
+	error := json.Unmarshal([]byte(testRule), &rule)
+
+	if error != nil {
+		return []data.Rule{}, error
+	}
+
+	rules = append(rules, rule)
+
+	return rules, nil
 }
 
 // UpdateOne always returns nil
