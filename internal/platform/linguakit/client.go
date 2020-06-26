92bdbb72fb874c425a2fc1b67a75fe741f1408c4
Douglas Figueroa <dffigueroa@galileo.edu>
2020-06-25 19:56:13 -0600

    bug(rules): commented code in linguakit client

--- internal/platform/linguakit/client.go
+++ internal/platform/linguakit/client.go
@@ -1,9 +1,15 @@
 package linguakit
 
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+)
+
 // AnalyzePhrase get the given phrase and decompose it in an array
 // of sentences
 func AnalyzePhrase(phrase string) ([]Sentence, error) {
-	/*var outbuf, errbuf bytes.Buffer
+	var outbuf, errbuf bytes.Buffer
 
 	cmd := exec.Command("linguakit", "dep", "es", phrase, "-s")
 	cmd.Stdout = &outbuf
@@ -15,12 +21,7 @@ func AnalyzePhrase(phrase string) ([]Sentence, error) {
 
 	if errorRunning != nil {
 		return []Sentence{}, fmt.Errorf("%s", errorString)
-	}*/
-
-	result := `SENT::el_DET_0_<gender:M|lemma:el|number:S|person:0|pos:0|possessor:0|token:el|type:A|> unknown_NOUN_1_<gender:0|lemma:unknown|number:0|person:3|pos:1|token:unknown|type:C|> ._SENT_2_<lemma:.|pos:2|token:.|>
-(SpecL;unknown_NOUN_1;el_DET_0)
----
-`
+	}
 
 	sentences, error := Parse(result)
 
