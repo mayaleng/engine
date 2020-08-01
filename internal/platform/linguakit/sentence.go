ebac190c07fa4af4095fb2aff523e3f7aaf5bb80
Douglas Figueroa <dffigueroa@galileo.edu>
2020-07-31 20:32:46 -0600

    feature(linguakit-api): POST method to get the analysis of linguakit

--- internal/platform/linguakit/sentence.go
+++ internal/platform/linguakit/sentence.go
@@ -1,7 +1,12 @@
 package linguakit
 
-// Sentence represente an array of words.
+// Sentence represents an array of words.
 type Sentence struct {
 	Pattern string
 	Words   []Word
+}
+
+// Analyze represents a sentence to proccess by Linguakit
+type Analyze struct {
+	Phrase string
 }
